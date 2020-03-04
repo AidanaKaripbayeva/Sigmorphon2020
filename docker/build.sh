a006bb9859ad99e4875f195b7ad6805c94227632
Bryan Lunt <bjlunt2@illinois.edu>
2020-03-04 01:01:42 -0600

    Improvements to docker image creation.

--- docker/build.sh
+++ docker/build.sh
@@ -8,4 +8,4 @@ pushd ${SCRIPT_DIR}
 
 docker build -t cs546_turkic_base ./base_software
 docker build --no-cache -t cs546_turkic_dataset ./with_dataset
-docker build -t cs546_turkic_user ./user_image
+docker build -t luntlab/cs546_turkic_user:latest -t cs546_turkic_user ./user_image
