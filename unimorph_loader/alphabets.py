cdaf188ea804293e0bb0e2bbe673e771ebf0cafe
Bryan Lunt <bjlunt2@illinois.edu>
2020-03-08 22:14:15 -0500

    Added most alphabets we are likely to see.

--- unimorph_loader/alphabets.py
+++ unimorph_loader/alphabets.py
@@ -1,13 +1,15 @@
-symbols = ' !"\'()*+,-./01235689:;=?@^_'
+symbols = ' !"\'()*+,-./0123456789:;=?@^_~'
 roman = 'abcdefghijklmnopqrstuvwxyz'
 latin_diacritic = 'ßàáâäåæèéêëìíîïðñòóôõöøùúûüýþāăąċčďđēĕęġīĭįļņŋōŏœŗšŧūŭųźžƿǟǣǫǿțȭȯȱȳɂɔɛʉʔ'
 turkish_latin = "abcçdefgğhıijklmnoöprsştuüvyz"
 cyrillic = "абвгдежзийклмнопрстуфхцчшщъыьэюяёіѣҥ"
-tones = "⁰¹²³⁴⁵"
-other = "|²³´¹ʼˀίӓӧӱӹᐟᵈᵊᵖḑḗạậẹệọộụ–’⁰⁴⁵
+tones = "⁰¹²³⁴⁵ᵈᵊᵖˀ"
+other = "|´ʼίӓӧӱӹᐟḑḗạậẹệọộụ–’
+
+#https://en.wikipedia.org/wiki/Kazakh_alphabets
+cyrillic_kazak = "аәбвгғдеёжзийкқлмнңоөпрстуұүфхһцчшщъыіьэюя"
 
 #https://en.wikipedia.org/wiki/Common_Turkic_Alphabet
-common_turkic_alphabet_upper = "AÄBCÇDEFGĞHIİJKLMNÑOÖPQRSŞTUÜVWXYZʼ"
-common_turkic_alphabet_lower = "aäbcçdefgğhıijklmnñoöpqrsştuüvwxyzʼ"
+common_turkic_alphabet = "aäbcçdefgğhıijklmnñoöpqrsştuüvwxyzʼ"
 common_turkic_ipa = "ɑæbdʒtʃdefgɣhɯiʒcklmnŋoøpqrsʃtuyvwxjzʔ"
 common_turkic_cyrillic = 'аәәебџчжддѕфгғҕһҳхыикқлљмнњңоөпрсҫшцттуүвўјзз́ҙ'
