68c5cfa04e96ba1cdf3f9f256d24a980b5891ed4
Bryan Lunt <bjlunt2@illinois.edu>
2020-03-08 21:08:38 -0500

    actually fixed.

--- unimorph_loader/uniload.py
+++ unimorph_loader/uniload.py
@@ -2,7 +2,7 @@
 import pandas as _pn
 
 def read_unimorph_tsv(filename):
-	return _pn.read_csv(filename,sep="\t",names=["lemma","form","features"],dtype=str)
+	return _pn.read_csv(filename,sep="\t",names=["lemma","form","features"],dtype=str,na_values=[],keep_default_na=False )
 
 MAX_LANGSPEC = 10
 
