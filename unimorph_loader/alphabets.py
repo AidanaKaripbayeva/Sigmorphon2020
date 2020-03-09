fd7fbbac1385f2ba98b8d867f0e2d2800594ab46
Bryan Lunt <bjlunt2@illinois.edu>
2020-03-08 22:05:10 -0500

    preparing for all alphebets we may encounter.

---
+++ unimorph_loader/alphabets.py
@@ -0,0 +1,13 @@
+symbols = ' !"\'()*+,-./01235689:;=?@^_'
+roman = 'abcdefghijklmnopqrstuvwxyz'
+latin_diacritic = 'ßàáâäåæèéêëìíîïðñòóôõöøùúûüýþāăąċčďđēĕęġīĭįļņŋōŏœŗšŧūŭųźžƿǟǣǫǿțȭȯȱȳɂɔɛʉʔ'
+turkish_latin = "abcçdefgğhıijklmnoöprsştuüvyz"
+cyrillic = "абвгдежзийклмнопрстуфхцчшщъыьэюяёіѣҥ"
+tones = "⁰¹²³⁴⁵"
+other = "|²³´¹ʼˀίӓӧӱӹᐟᵈᵊᵖḑḗạậẹệọộụ–’⁰⁴⁵
+
+#https://en.wikipedia.org/wiki/Common_Turkic_Alphabet
+common_turkic_alphabet_upper = "AÄBCÇDEFGĞHIİJKLMNÑOÖPQRSŞTUÜVWXYZʼ"
+common_turkic_alphabet_lower = "aäbcçdefgğhıijklmnñoöpqrsştuüvwxyzʼ"
+common_turkic_ipa = "ɑæbdʒtʃdefgɣhɯiʒcklmnŋoøpqrsʃtuyvwxjzʔ"
+common_turkic_cyrillic = 'аәәебџчжддѕфгғҕһҳхыикқлљмнњңоөпрсҫшцттуүвўјзз́ҙ'
