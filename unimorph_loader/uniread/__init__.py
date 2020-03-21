812976400585439891e14221317adb2ebbcbddff
Bryan Lunt <bjlunt2@illinois.edu>
2020-03-21 16:36:16 -0500

    better schema loading

--- unimorph_loader/uniread/__init__.py
+++ unimorph_loader/uniread/__init__.py
@@ -1,6 +1,7 @@
-from provided import *
+from . import schema
 
 import pandas as _pn
+
 
 def read_unimorph_tsv(filename):
 	return _pn.read_csv(filename,sep="\t",names=["lemma","form","features"],dtype=str,na_values=[],keep_default_na=False )
--- unimorph_loader/uniread/schema.py
+++ unimorph_loader/uniread/schema.py
@@ -2,7 +2,7 @@ import yaml as _yml
 
 from collections import OrderedDict
 
-def load_unimorph_tags(filename):
+def load_unimorph_schema_from_yaml(filename):
 	tag_list = None
 	with open(filename,"r") as infile:
 		tag_list = _yml.safe_load(infile)
@@ -14,6 +14,9 @@ def load_unimorph_tags(filename):
 		tags_for_cat = list() #makeshift ordered set and back to list
 		for one_or_more_tags in tag_list["categories"][cat]:
 			tags_for_cat.extend(one_or_more_tags.replace("/","+").split("+") )
+		
+		tags_for_cat = [i for i in tags_for_cat if not (i.startswith("non{") and i.endswith("}"))]
+		#Using as an ordered set
 		thetags[cat] = list(OrderedDict([(i,True) for i in tags_for_cat]).keys())
 	#
 	return thetags
