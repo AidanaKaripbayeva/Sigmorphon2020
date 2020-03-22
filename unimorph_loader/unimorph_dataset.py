aa3e9ecfce8ac1a9f01cb0c2058457e3a19588e7
Bryan Lunt <bjlunt2@illinois.edu>
2020-03-21 22:05:56 -0500

    Dataset Done. Now we need a dataloader/collator.

--- unimorph_loader/unimorph_dataset.py
+++ unimorph_loader/unimorph_dataset.py
@@ -18,21 +18,23 @@ END_TOKEN_CHAR = chr(3) #ASCII END OF TEXT
 
 from . import alphabets #relative import, only way to get the module to work
 
-
-
 class UnimorphDataset(torch.utils.data.Dataset):
     """
     """
-    def __init__(self, family_tensor, language_tensor, tags_tensor, lemma_tensor_list, form_tensor_list ):
+    def __init__(self, family_tensor, language_tensor, tags_tensor, lemma_tensor_list, form_tensor_list, tags_str_list, lemmata_str_list, forms_str_list ):
         super(UnimorphDataset,self).__init__()
         self.families = family_tensor
         self.languages = language_tensor
         self.tags = tags_tensor
         self.lemmata = lemma_tensor_list
         self.forms =  form_tensor_list
+        self.tags_str = tags_str_list
+        self.lemmata_str = lemmata_str_list
+        self.forms_str = forms_str_list
     
     def __getitem__(self, index):
-        return (self.families[index], self.languages[index], self.tags[index], self.lemmata[index], self.forms[index])
+        return (self.families[index], self.languages[index], self.tags[index], self.lemmata[index], self.forms[index],
+                                self.tags_str[index], self.lemmata_str[index], self.forms_str[index])
     
     def __add__(self, other):
         return UnimorphDataset(
@@ -40,21 +42,31 @@ class UnimorphDataset(torch.utils.data.Dataset):
                                 torch.cat([self.languages, other.languages]),
                                 torch.cat([self.self.tags,other.tags]),
                                 self.lemmata + other.lemmata,
-                                self.forms + other.forms
+                                self.forms + other.forms,
+                                self.tags_str + other.tags_str,
+                                self.lemmata_str + other.lemmata_str,
+                                self.forms_str + other.forms_str
                                 )
 
+from . import uniread
 group_splitter = re.compile(";")
 tag_splitter = re.compile("/|\+")
 non_detector = re.compile("non{(.*)}")
 class UnimorphTagOneHotConverter(object):
+    
+    @classmethod
+    def from_schemafile(blah,filename):
+        a_schema = uniread.schema.load_unimorph_schema_from_yaml(filename)
+        return UnimorphTagOneHotConverter(a_schema)
+    
     def __init__(self,schema):
         self.schema = schema
         self.tag_to_id = OrderedDict()
         self.tag_to_group = dict()
 
-        for i,group in enumerate(a_schema):
-            for j,tag in enumerate(a_schema[group]):
-                self.tag_to_id[tag] = len(tag_to_id)
+        for i,group in enumerate(self.schema):
+            for j,tag in enumerate(self.schema[group]):
+                self.tag_to_id[tag] = len(self.tag_to_id)
                 self.tag_to_group[tag] = group
     
     def __call__(self, tagstring):
@@ -77,7 +89,7 @@ class UnimorphTagOneHotConverter(object):
         return hot_vector
                 
         
-def pandas_to_dataset(dataset_or_sets,tag_converter=None,alphabet_converter_in=None,alphabet_converter_out=None):
+def pandas_to_dataset(dataset_or_sets,tag_converter=UnimorphTagOneHotConverter.from_schemafile("./tags.yaml"),alphabet_converter_in=None,alphabet_converter_out=None):
     if isinstance(dataset_or_sets, pandas.DataFrame):
         dataset_or_sets = [dataset_or_sets]
     assert isinstance(dataset_or_sets, list)
@@ -105,5 +117,8 @@ def pandas_to_dataset(dataset_or_sets,tag_converter=None,alphabet_converter_in=N
     lemma_tensor_list = [ torch.LongTensor(alphabet_converter_in(i)) for i in total_dataframe["lemma"] ]
     form_tensor_list = [ torch.LongTensor(alphabet_converter_out(i)) for i in total_dataframe["form"] ]
     
-    return UnimorphDataset(family_tensor, lang_tensor, tags_tensor, lemma_tensor_list, form_tensor_list)
+    return UnimorphDataset(family_tensor, lang_tensor,
+                    tags_tensor, lemma_tensor_list, form_tensor_list,
+                    total_dataframe["tags"].to_list(), total_dataframe["lemma"].to_list(), total_dataframe["form"].to_list()
+                    )
     
