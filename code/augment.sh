8efdcb2b227c216cba2d2881e3f8b15f0e5036f5
garlicrybread <garlicrybread@gmail.com>
2020-03-30 16:51:19 -0500

    first steps toward a better hallucination

---
+++ code/augment.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+FILE=~/Documents/UIUC/2020_Spring/CS546/sigmorphon2020/TurkicSigmorphon2020/code/hallucination/
+LANG_PATH=~/Documents/UIUC/2020_Spring/CS546/sigmorphon2020/neural-transducer/task0-data-master
+
+#for lng in mlg ceb hil tgl mao; do
+#python ${FILE}/augment.py ${LANG_PATH}/austronesian $lng --examples 10000
+#done
+
+for lng in eng; do # in dan isl nob swe nld eng deu gmh frr ang; do
+python ${FILE}/augment.py ${LANG_PATH}/germanic $lng --examples 10000
+done
+
+#for lng in nya kon lin lug sot swa zul aka gaa; do
+#python ${FILE}/augment.py ${LANG_PATH}/niger-congo $lng --examples 10000
+#done
+
+#for lng in cpa azg xty zpv ctp czn cly otm ote pei; do
+#python ${FILE}/augment.py ${LANG_PATH}/oto-manguean $lng --examples 10000
+#done
+
+#for lng in est fin izh krl liv vep vot mhr myv mdf sme; do
+#python ${FILE}/augment.py ${LANG_PATH}/uralic $lng --examples 10000
+#done
---
+++ code/hallucination/hall_choice.py
@@ -0,0 +1,9 @@
+import sys
+#sys.path.append('data/uniread')
+from data.uniread import read_unimorph_tsv
+sys.path.append('src')
+
+def frequency_choice():
+    dummy = "a"
+    read_unimorph_tsv(family_alphabets.tsv)
+    return dummy
