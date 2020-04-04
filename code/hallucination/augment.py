dd67fa3b48adc51a0ca134b0af991d03d738e2be
garlicrybread <garlicrybread@gmail.com>
2020-04-04 12:35:38 -0400

    completed hallucination using frequency probabilities

--- code/hallucination/augment.py
+++ code/hallucination/augment.py
@@ -2,7 +2,7 @@
 Borrowed from https://github.com/antonisa/inflection
 '''
 import sys
-sys.path.append('src')
+sys.path.insert(0, 'src')
 
 import align
 import argparse
@@ -11,6 +11,8 @@ import os
 from random import random, choice
 import re
 from hall_choice import frequency_choice
+
+import data.languages
 
 
 def read_data(filename):
@@ -54,7 +56,10 @@ def find_good_range(a,b):
     return ranges
 
 
-def augment(inputs, outputs, tags, characters):
+def augment(inputs, outputs, tags, characters, lang_family, L2):
+    print(lang_family, L2)
+    lang_alphabet = load_the_alphabet(lang_family, L2)
+    
     temp = [(''.join(inputs[i]), ''.join(outputs[i])) for i in range(len(outputs))]
     aligned = align.Aligner(temp, align_symbol=' ').alignedpairs
 
@@ -85,10 +90,11 @@ def augment(inputs, outputs, tags, characters):
                     # define the new function
                     if random() > 0.5: #arbitrary value
                         #nc = choice(vocab)
-                        nc = frequency_choice()
+                        nc = frequency_choice(lang_alphabet)
                         #print(nc)
                         new_i[j] = nc
                         new_o[j] = nc
+                       
             new_i1 = [c for l,c in enumerate(new_i) if (c.strip() or (new_o[l]==' ' and new_i[l] == ' '))]
             new_o1 = [c for l,c in enumerate(new_o) if (c.strip() or (new_i[l]==' ' and new_o[l] == ' '))]
             new_inputs.append(new_i1)
@@ -101,56 +107,65 @@ def augment(inputs, outputs, tags, characters):
 
     return new_inputs, new_outputs, new_tags
 
+def load_the_alphabet(lang_family, lang):
+    lang_collection = data.languages.LanguageCollection.get_default(alphabet_choice="individual")
+    lang_alphabet = lang_collection[lang_family][lang].alphabet
+    return lang_alphabet
+
 def get_chars(l):
     flat_list = [char for word in l for char in word]
     return list(set(flat_list))
 
+def main_augment_run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("datapath", help="path to data", type=str)
+    parser.add_argument("language_family", help="language_family", type=str)
+    parser.add_argument("language", help="language", type=str)
+    
+    parser.add_argument("--examples", help="number of hallucinated examples to create (def: 10000)", default=10000, type=int)
+    parser.add_argument("--use_dev", help="whether to use the development set (def: False)", action="store_true")
+    args = parser.parse_args()
+    
+    LANG_FAM = args.language_family
+    DATA_PATH = args.datapath + "/" + LANG_FAM
+    
+    L2 = args.language
+    LOW_PATH = os.path.join(DATA_PATH, L2+".trn")
+    DEV_PATH = os.path.join(DATA_PATH, L2+".dev")
+    
+    N = args.examples
+    usedev = args.use_dev
 
-parser = argparse.ArgumentParser()
-parser.add_argument("datapath", help="path to data", type=str)
-parser.add_argument("language", help="language", type=str)
-parser.add_argument("--examples", help="number of hallucinated examples to create (def: 10000)", default=10000, type=int)
-parser.add_argument("--use_dev", help="whether to use the development set (def: False)", action="store_true")
-args = parser.parse_args()
+    lowi, lowo, lowt = read_data(LOW_PATH)
+    devi, devo, devt = read_data(DEV_PATH)
 
-DATA_PATH = args.datapath
-L2 = args.language
-LOW_PATH = os.path.join(DATA_PATH, L2+".trn")
-DEV_PATH = os.path.join(DATA_PATH, L2+".dev")
-
-N = args.examples
-usedev = args.use_dev
-
-lowi, lowo, lowt = read_data(LOW_PATH)
-devi, devo, devt = read_data(DEV_PATH)
-
-if usedev:
-    vocab = get_chars(lowi+lowo+devi+devo)
-else:
-    vocab = get_chars(lowi+lowo)
-
-i,o,t = [], [], []
-while len(i) < N:
     if usedev:
-        # Do augmentation also using examples from dev
-        ii,oo,tt = augment(devi+lowi, devo+lowo, devt+lowt, vocab)
+        vocab = get_chars(lowi+lowo+devi+devo)
     else:
-        # Just augment the training set
-        ii,oo,tt = augment(lowi, lowo, lowt, vocab)
-    ii = [c for c in ii if c]
-    oo = [c for c in oo if c]
-    tt = [c for c in tt if c]
-    i += ii
-    o += oo
-    t += tt
-    if len(ii) == 0:
-        break
+        vocab = get_chars(lowi+lowo)
 
-# Wait is this needed?
-i = [c for c in i if c]
-o = [c for c in o if c]
-t = [c for c in t if c]
+    i,o,t = [], [], []
+    while len(i) < N:
+        if usedev:
+            # Do augmentation also using examples from dev
+            ii,oo,tt = augment(devi+lowi, devo+lowo, devt+lowt, vocab, LANG_FAM, L2)
+        else:
+            # Just augment the training set
+            ii,oo,tt = augment(lowi, lowo, lowt, vocab, LANG_FAM, L2)
+        ii = [c for c in ii if c]
+        oo = [c for c in oo if c]
+        tt = [c for c in tt if c]
+        i += ii
+        o += oo
+        t += tt
+        if len(ii) == 0:
+            break
 
-with codecs.open(os.path.join(DATA_PATH,L2+".hall"), 'w', 'utf-8') as outp:
-    for k in range(min(N, len(i))):
-        outp.write(''.join(i[k]) + '\t' + ''.join(o[k]) + '\t' + ';'.join(t[k]) + '\n')
+    # Wait is this needed?
+    i = [c for c in i if c]
+    o = [c for c in o if c]
+    t = [c for c in t if c]
+
+    with codecs.open(os.path.join(DATA_PATH,L2+".hall"), 'w', 'utf-8') as outp:
+        for k in range(min(N, len(i))):
+            outp.write(''.join(i[k]) + '\t' + ''.join(o[k]) + '\t' + ';'.join(t[k]) + '\n')
--- code/hallucination/hall_choice.py
+++ code/hallucination/hall_choice.py
@@ -1,9 +1,24 @@
 import sys
+from ast import literal_eval
 #sys.path.append('data/uniread')
-from data.uniread import read_unimorph_tsv
-sys.path.append('src')
+#from data.uniread import read_unimorph_tsv
+# import data.languages
+from random import choices
+#sys.path.append('src')
 
-def frequency_choice():
-    dummy = "a"
-    read_unimorph_tsv(family_alphabets.tsv)
-    return dummy
+def frequency_choice(lang_alphabet):
+    # lang_collection = data.languages.LanguageCollection.get_default(alphabet_choice="individual")
+    # lang_alphabet = lang_collection[lang_family][lang].alphabet
+    count = lang_alphabet.counts
+    total = sum(count.values())
+    probabilities = {key: count[key]/total for key in count.keys()}
+    prob_key = list(probabilities.keys())
+    prob_value = list(probabilities.values())
+    #print(sum(probabilities.values()))
+    if round(sum(probabilities.values()),5) != 1.0:
+        print(round(sum(probabilities.values()),5))
+        assert sum(probabilities.values()) == 1.0 # they should all add up to one
+    
+    letter = str(choices(prob_key, prob_value)[0])
+    return letter 
+
