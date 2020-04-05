1ce5281be46fe4b0947d76c8cae75d0234efcd73
Bryan J. Lunt <bjlunt2@illinois.edu>
2020-04-05 16:52:21 -0500

    added PBS directives to run script

--- run_bw.sh
+++ run_bw.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#PBS -l nodes=1:ppn=1:xk,walltime=5:00:00
+#PBS -q normal
+
 module load gcc/5.3.0 cmake/3.9.4 cudatoolkit/9.1.85_3.10-1.0502.df1cc54.3.1 cray-libsci/18.12.1 
 source /projects/eot/bbcj/bjlunt2/opt/miniconda3/bin/activate
 source activate torch
@@ -10,7 +13,9 @@ BATCH_SIZE=8
 
 which python
 
-aprun -d 8 -n1 -N1 python code/main.py --export-dir $EXPORT_DIR --sigmorphon2020-root $DATA_ROOT --batch-size $BATCH_SIZE --languages eng \
+cd ${PBS_O_WORKDIR}
+
+aprun -d ${PBS_NUM_PPN} -n1 -N1 python code/main.py --export-dir $EXPORT_DIR --sigmorphon2020-root $DATA_ROOT --batch-size $BATCH_SIZE --languages eng \
 --language-info-file ./code/data/languages/individual_alphabets.tsv \
 --model dummy \
 --adadelta-lr 5.0 
