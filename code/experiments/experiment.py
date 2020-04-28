b6d93f4969976d704c3c20c689f1417e0fe15f91
Bryan Lunt <bjlunt2@illinois.edu>
2020-04-27 20:29:30 -0500

    forgot to commit

--- code/experiments/experiment.py
+++ code/experiments/experiment.py
@@ -26,9 +26,10 @@ class Experiment:
     """
 
     class AutoPaddedLoss(object):
-        def __init__(self, loss, pad_index=Alphabet.stop_integer):
+        def __init__(self, loss, pad_index=Alphabet.stop_integer, device=None):
             self.loss = loss
             self.pad_index = pad_index
+            self.device = device
         
         def __call__(self, probs, target):
             L_probs = probs.shape[0]
@@ -36,7 +37,7 @@ class Experiment:
             
             if L_probs < L_target:
                 #TODO: Fewer assumptions about the shape of probs
-                extra_probs = torch.zeros(L_target-L_probs, probs.shape[1])
+                extra_probs = torch.zeros(L_target-L_probs, probs.shape[1],device=self.device)
                 extra_probs[:,self.pad_index] = 1
                 probs = torch.cat([probs, extra_probs])
                 
@@ -44,9 +45,12 @@ class Experiment:
             if L_target < L_probs:
                 target = torch.cat([target] + [torch.full((L_probs-L_target,),self.pad_index,dtype=torch.long)])
             
+            probs = probs.to(self.device)
+            target = probs.to(self.device)
             return self.loss(probs,target)
         
         def to(self, target_device):
+            self.device = target_device
             self.loss = self.loss.to(target_device)
             return self
 
