7809e879a6345349c340119d8eb439791bb96d55
Bryan Lunt <bjlunt2@illinois.edu>
2020-04-28 01:57:59 -0500

    progress. Huge mess.

--- code/models/dumbertransducer.py
+++ code/models/dumbertransducer.py
@@ -7,8 +7,37 @@ import torch
 
 import torch.nn.utils.rnn as _rnn_utils
 
+class EncodingWindows(torch.nn.Module):
+    def __init__(self, before, after):
+        super().__init__()
+        self.before = before
+        self.after = after
+    
+    def forward(self, padded_data):
+        #TODO: This is probably quite slow
+        
+        is_packed = isinstance(padded_data,_rnn_utils.PackedSequence)
+        if is_packed:
+            padded_data, lens = _rnn_utils.pad_packed_sequence(padded_data,padding_value=Alphabet.stop_integer)
+        
+        y = torch.cat([padded_data[0:1] for i in range(self.before)] + [padded_data] + [padded_data[-1:] for i in range(self.after)])
+        y = y.unfold(0,self.before+1+self.after,1)
+        if padded_data.ndim == 1:
+            pass
+        elif padded_data.ndim == 2:
+            y = y.reshape(y.shape[0],-1)
+        elif padded_data.ndim >= 3:
+            newshape = [int(i) for i in y.shape[:-2]] + [-1]
+            y = y.reshape(*newshape)
+        
+        if is_packed:
+            return _rnn_utils.pack_padded_sequence(y, lens, batch_first=False, enforce_sorted=False)
+        return y
+        #return y.unfold(0,self.before+1+self.after,1)
+
+
 class DumberEncoder(torch.nn.Module):
-    def __init__(self, alphabet_size, embedding_dim=40, hidden_dim=10, num_layers=1,num_languages=None):
+    def __init__(self, alphabet_size, embedding_dim=40, hidden_dim=10, num_layers=3, num_languages=None):
         super().__init__()
         #PARAMETERS
         # The number of different letters in the alphabet.
@@ -18,22 +47,48 @@ class DumberEncoder(torch.nn.Module):
         # Then number of different languages.
         self.num_languages = num_languages
         
+        self.hidden_dim = [ hidden_dim ]*num_layers if isinstance(hidden_dim, int) else hidden_dim
+        assert isinstance(self.hidden_dim, list) or isinstance(self.hidden_dim, tuple), str(type(self.hidden_dim))
+        assert len(self.hidden_dim) == num_layers, "Either provide one integer for hidden_dim, or one for each layer"
+        
         #LAYERS
         self.position = BS.RNN_Position_Encoding(20,20)
         # Create the embedding layer for the encoder.
         self.embedding = BRNN.RNN_Input_Embedding(torch.nn.Embedding(alphabet_size, embedding_dim))
         
+        self.windows = EncodingWindows(1,1)
+        
+        
         # Forward LSTM for encoding
-        self.f_lstm = torch.nn.LSTM(embedding_dim, hidden_dim)
+        #self.f_lstm = torch.nn.LSTM(embedding_dim*3, hidden_dim)
+        
+        #Layers, totally parallel, sort of.
+        tmp_layers = [torch.nn.Linear(embedding_dim*3, self.hidden_dim[0]), torch.nn.Sigmoid()]
+        for i in range(1,num_layers):
+            tmp_layers.append(torch.nn.Linear(self.hidden_dim[i-1], self.hidden_dim[i]))
+            tmp_layers.append(torch.nn.Sigmoid())
+        self.f_lstm = BRNN.RNN_FFLayer(torch.nn.Sequential(*tmp_layers))
     
     def forward(self, families, languages, tags, lemma):
         pos = self.position(lemma)#I expect it to blow up here.
         emb,_ = self.embedding(lemma)
         
-        lstm_output, lstm_hidden = self.f_lstm(emb)
+        window_emb = self.windows(emb)
+        
+        lstm_output, lstm_hidden = self.f_lstm(window_emb)
         
         return pos, emb, lstm_output, lstm_hidden
         
+
+class KeyValueAttention(torch.nn.Module):
+    def __init__(self, ff_layer):
+        super().__init__()
+        self.ff = ff_layer
+        self.softmax = torch.nn.Softmax(-1)
+    
+    def forward(self, q, k, v):
+        x = self.ff(q)
+        return self.softmax(x@k.T)@v
 
 class DumberDecoder(torch.nn.Module):
     def __init__(self, alphabet_size, tag_vector_dim, embedding_dim=40, hidden_dim=10, num_layers=3, output_length=50, num_languages=None):
@@ -43,6 +98,12 @@ class DumberDecoder(torch.nn.Module):
         self.linear = torch.nn.Linear(hidden_dim, alphabet_size) #self.linear = torch.nn.Linear(embedding_dim, alphabet_size)
         self.sigmoid = torch.nn.Sigmoid()
         self.final_softmax = torch.nn.Softmax(dim=-1)
+        
+        self.position = BS.RNN_Position_Encoding(20,20)
+        
+        self.attention = KeyValueAttention(
+                torch.nn.Sequential(torch.nn.Linear(42,42), torch.nn.Sigmoid())
+            )
     
     def forward(self, family, language, tags, position, embedding, states, hidden):
         
@@ -115,7 +176,7 @@ class DumberTransducer(torch.nn.Module):
         lengths = [len(i) for i in lemmata]
         
         #DEVICE MOVEMENT
-        if None is not self.device:
+        if self.device is not None:
             families = [i.to(self.device) for i in families]
             languages = [i.to(self.device) for i in languages]
             tags = [i.to(self.device) for i in tags]
@@ -143,7 +204,8 @@ class DumberTransducer(torch.nn.Module):
             all_hiddens_for_single_item = tuple([h[:,i,:] for h in hidden])
             
             y = self.decoder(families[i], languages[i], tags[i],
-                                padded_pos[:,i,:], padded_emb[:,i,:], padded_x[:,i,:],
+                                padded_pos[:,i,:],
+                                padded_emb[:,i,:], padded_x[:,i,:],
                                 all_hiddens_for_single_item
                             )
             
