2da39b542453c5e1e154e59fc19d6d608d9e736b
Bryan Lunt <bjlunt2@illinois.edu>
2020-05-07 17:19:54 -0500

    more cleanup, speedup

--- code/models/dumbertransducer/__init__.py
+++ code/models/dumbertransducer/__init__.py
@@ -11,8 +11,8 @@ from .attention import *
 from .windows import *
 
 
-class DumberEncoder(torch.nn.Module):
-    def __init__(self, alphabet_size, embedding_dim=40, hidden_dim=10, num_layers=3, num_languages=None):
+class LetterContextEncoder(torch.nn.Module):
+    def __init__(self, alphabet_size, embedding_dim=40, hidden_dim=10, num_layers=3, context_before=2,context_after=1,context_dim=20,num_languages=None):
         super().__init__()
         #PARAMETERS
         # The number of different letters in the alphabet.
@@ -22,37 +22,50 @@ class DumberEncoder(torch.nn.Module):
         # Then number of different languages.
         self.num_languages = num_languages
         
+        self.context_before = context_before
+        self.context_after = context_after
+        self.context_dim = context_dim
+        
         self.hidden_dim = [ hidden_dim ]*num_layers if isinstance(hidden_dim, int) else hidden_dim
         assert isinstance(self.hidden_dim, list) or isinstance(self.hidden_dim, tuple), str(type(self.hidden_dim))
         assert len(self.hidden_dim) == num_layers, "Either provide one integer for hidden_dim, or one for each layer"
         
         #LAYERS
-        self.position = BS.RNN_Position_Encoding(20,20)
         # Create the embedding layer for the encoder.
         self.embedding = BRNN.RNN_Input_Embedding(torch.nn.Embedding(alphabet_size, embedding_dim))
         
-        self.windows = EncodingWindows(1,1)
+        self.context_embedder = torch.nn.Sequential(torch.nn.Linear(self.embedding_dim, self.context_dim))
+        
+        self.before_context = BRNN.RNN_FFLayer(torch.nn.Sequential(
+                                            self.context_embedder,
+                                            OffsetWindows(-context_before,True),
+                                            torch.nn.Linear(context_before* self.context_dim, self.context_dim),
+                                            torch.nn.Sigmoid()
+                                        ))
+        self.after_context  = BRNN.RNN_FFLayer(torch.nn.Sequential(
+                                            self.context_embedder,
+                                            OffsetWindows(context_after,True),
+                                            torch.nn.Linear(context_after*self.context_dim, self.context_dim),
+                                            torch.nn.Sigmoid()
+                                        ))
+        self.all_context = BRNN.RNN_FFLayer(torch.nn.Sequential(
+                                            torch.nn.Linear(context_dim*2, context_dim),
+                                            torch.nn.Sigmoid()
+                                        ))
         
         
-        # Forward LSTM for encoding
-        #self.f_lstm = torch.nn.LSTM(embedding_dim*3, hidden_dim)
-        
-        #Layers, totally parallel, sort of.
-        tmp_layers = [torch.nn.Linear(embedding_dim*3, self.hidden_dim[0]), torch.nn.Sigmoid()]
-        for i in range(1,num_layers):
-            tmp_layers.append(torch.nn.Linear(self.hidden_dim[i-1], self.hidden_dim[i]))
-            tmp_layers.append(torch.nn.Sigmoid())
-        self.f_lstm = BRNN.RNN_FFLayer(torch.nn.Sequential(*tmp_layers))
+        #self.f_lstm = BRNN.RNN_FFLayer(torch.nn.Sequential(*tmp_layers))
     
     def forward(self, families, languages, tags, lemma):
-        pos = self.position(lemma)#I expect it to blow up here.
         emb,_ = self.embedding(lemma)
         
-        window_emb = self.windows(emb)
+        b4,_ = self.before_context(emb)
+        aft,_ = self.after_context(emb)
         
-        lstm_output, lstm_hidden = self.f_lstm(window_emb)
+        ctxt,_ = self.all_context(torch.cat([b4, aft],dim=2))
         
-        return pos, emb, lstm_output, lstm_hidden
+        return emb, ctxt, tuple()
+        
         
 
 class DumberDecoder(torch.nn.Module):
@@ -71,7 +84,7 @@ class DumberDecoder(torch.nn.Module):
                 torch.nn.Sequential(torch.nn.Linear(42,42), torch.nn.Sigmoid())
             )
     
-    def forward(self, family, language, tags, position, embedding, states, hidden):
+    def forward(self, family, language, tags, embedding, states, hidden, tf_forms=None):
         
         output_sequence = torch.zeros(self.max_output_length, dtype=torch.int32)
         output_probs = torch.zeros((self.max_output_length, self.alphabet_size))
@@ -83,27 +96,18 @@ class DumberDecoder(torch.nn.Module):
         output_probs[1:,Alphabet.stop_integer] = 1.0
         
         #Need to change the extents of this loop.
-        for output_i in range(1, min(self.max_output_length, embedding.shape[0]) ):
-            
-            #one_attended_emb = self.pos_attention(position[output_i], position, embedding)
-            #one_attended_state = self.pos_attention(position[output_i], position, states)
-            
-            #import pdb; pdb.set_trace()
-            #x = self.linear(torch.cat([output_probs[output_i-1], embedding[output_i], states[output_i]])) #x = self.linear(embedding[output_i])
-            x = self.linear(embedding[output_i])
-            x = self.sigmoid(x)
-            y = self.final_softmax(x)
-            
-            output_symbol = y.argmax(-1)
-            
-            output_sequence[output_i] = output_symbol
-            output_probs[output_i,:] = y
-            
-            if output_symbol == Alphabet.stop_integer:
-                break
         
+        #faster? FASTER!
+        x = self.linear(embedding)
+        x = self.sigmoid(x)
+        y = self.final_softmax(x)
+        output_symbols=y.argmax(-1)
+        output_probs[:y.shape[0]] = y
         
-        return output_probs[:output_i+1] #no probs past predicted end.
+        for output_i in range(1, min(self.max_output_length, embedding.shape[0])):
+            if(output_symbols[output_i] == Alphabet.stop_integer):
+                return output_probs[:output_i]
+        return output_probs[:output_i]
         
         
 
@@ -126,7 +130,7 @@ class DumberTransducer(torch.nn.Module):
         self.output_length = output_length
         
         
-        self.encoder = DumberEncoder(alphabet_size, embedding_dim)
+        self.encoder = LetterContextEncoder(alphabet_size, embedding_dim)
         
         self.decoder = DumberDecoder(alphabet_size, tag_vector_dim, embedding_dim)
         
@@ -136,13 +140,41 @@ class DumberTransducer(torch.nn.Module):
         self.device = target_device
         super().to(self.device)
         return self
-
-    def forward(self, families, languages, tags, lemmata, teacher_forcing=None):
+        
+    def do_decode(self, families, languages, tags, emb, rnn_states,
+                        hidden_states, tf_forms=None):
+        
         batch_size = len(tags)
         batch_outputs = []  # Will hold the integral representation of the characters to be output for this batch.
         batch_probabilities = []   # Will hold the probability vectors for this batch.
         
-        assert isinstance(lemmata, list) or isinstance(lemmata, tuple), str(type(lemmata)) + " Only lists are implemented"
+        # For each input in this batch ...
+        for i in range(batch_size):
+            
+            all_hiddens_for_single_item = tuple([h[:,i,:] for h in hidden_states])
+            
+            y = self.decoder(
+                                families[:,i],
+                                languages[:,i], tags[i],
+                                emb[:,i,:], rnn_states[:,i,:],
+                                all_hiddens_for_single_item,
+                                tf_forms=tf_forms
+                            )
+            
+            outputs = y.argmax(-1)#y was already stacked.
+            
+            # Put the results in the list.
+            batch_probabilities.append(y)
+            batch_outputs.append(outputs)
+        
+        return batch_probabilities, batch_outputs
+    
+    def forward(self, families, languages, tags, lemmata, tf_forms=None):
+        batch_size = len(tags)
+        batch_outputs = []  # Will hold the integral representation of the characters to be output for this batch.
+        batch_probabilities = []   # Will hold the probability vectors for this batch.
+        
+        assert isinstance(lemmata, list) or isinstance(lemmata, tuple), "Type was: " + str(type(lemmata)) + " but only lists are implemented"
         lengths = [len(i) for i in lemmata]
         
         #DEVICE MOVEMENT #TODO: move out of here.
@@ -151,39 +183,22 @@ class DumberTransducer(torch.nn.Module):
             languages = [i.to(self.device) for i in languages]
             tags = [i.to(self.device) for i in tags]
             lemmata = [i.to(self.device) for i in lemmata]
+            if tf_forms is not None:
+                assert isinstance(tf_forms, list), "only lists are implemented"
+                tf_forms = [t.to(self.device) for t in tf_forms]
         
         #
         #The encoder uses layers that can do everything at once.
         #ENCODE EVERYTHING
         #
-        packed_fam   = _rnn_utils.pack_sequence(families,  enforce_sorted=False)
-        packed_langs = _rnn_utils.pack_sequence(languages, enforce_sorted=False)
-        packed_lems  = _rnn_utils.pack_sequence(lemmata,   enforce_sorted=False)
+        padded_fam = _rnn_utils.pad_sequence(families, batch_first=False, padding_value=Alphabet.stop_integer)
+        padded_langs = _rnn_utils.pad_sequence(languages, batch_first=False, padding_value=Alphabet.stop_integer)
+        padded_lems = _rnn_utils.pad_sequence(lemmata, batch_first=False, padding_value=Alphabet.stop_integer)
         
-        packed_pos, packed_emb, packed_x, hidden = self.encoder(packed_fam, packed_langs, tags, packed_lems)
+        padded_emb, padded_x, hidden = self.encoder(padded_fam, padded_langs, tags, padded_lems)
         
-        padded_pos, pos_ls = _rnn_utils.pad_packed_sequence(packed_pos)
-        padded_emb, emb_ls = _rnn_utils.pad_packed_sequence(packed_emb)
-        padded_x,     x_ls = _rnn_utils.pad_packed_sequence(packed_x)
-        
-        assert (pos_ls == emb_ls).all() and (emb_ls == x_ls).all(), "everything must be the same length"
-        
-        # For each input in this batch ...
-        for i in range(batch_size):
-            
-            all_hiddens_for_single_item = tuple([h[:,i,:] for h in hidden])
-            
-            y = self.decoder(families[i], languages[i], tags[i],
-                                padded_pos[:,i,:],
-                                padded_emb[:,i,:], padded_x[:,i,:],
-                                all_hiddens_for_single_item
-                            )
-            
-            outputs = y.argmax(-1)#y was already stacked.
-            
-            # Put the results in the list.
-            batch_probabilities.append(y)
-            batch_outputs.append(outputs)
+        batch_probabilities, batch_outputs = self.do_decode(padded_fam, padded_langs, tags, padded_emb, padded_x,
+                            hidden, tf_forms=tf_forms)
             
         # Report the results of this batch.
         return batch_probabilities
--- code/models/dumbertransducer/windows.py
+++ code/models/dumbertransducer/windows.py
@@ -38,5 +38,43 @@ class EncodingWindows(torch.nn.Module):
         return y
         #return y.unfold(0,self.before+1+self.after,1)
 
+class OffsetWindows(torch.nn.Module):
+    def __init__(self, offset,inclusive=False):
+        super().__init__()
+        self.offset = offset
+        self.inclusive=inclusive
+    
+    def forward(self, padded_data):
+        #TODO: This is probably quite slow
+        
+        is_packed = isinstance(padded_data,_rnn_utils.PackedSequence)
+        if is_packed:
+            padded_data, lens = _rnn_utils.pad_packed_sequence(padded_data,padding_value=Alphabet.stop_integer)
+        
+        cat_list = list()
+        
+        stacking_dim = 1
+        if padded_data.ndim >= 3:
+            stacking_dim = 2
+            
+        if self.offset < 0:
+            y = torch.cat([padded_data[0:1]]*(-1*self.offset) + [padded_data[:self.offset]], dim=0)
+            if self.inclusive and self.offset < -1:
+                stack_these = [y] + [torch.cat([padded_data[0:1]]*(-1*o) + [padded_data[:o]], dim=0) for o in range(self.offset+1,0)]
+                y = torch.cat(stack_these, dim=stacking_dim)
+        elif self.offset > 0:
+            y = torch.cat([padded_data[self.offset:]] + [padded_data[-1:]]*self.offset, dim=0)
+            if self.inclusive and self.offset > 1:
+                stack_these = [torch.cat([padded_data[o:]] + [padded_data[-1:]]*o, dim=0) for o in range(1,self.offset)] + [y]
+                y = torch.cat(stack_these, dim=stacking_dim)
+        else:
+            y = padded_data
+        
+        
+        if is_packed:
+            return _rnn_utils.pack_padded_sequence(y, lens, batch_first=False, enforce_sorted=False)
+        return y
+        #return y.unfold(0,self.before+1+self.after,1)
 
-__all__ = ["Encodingwindows"]
+
+__all__ = ["EncodingWindows","OffsetWindows"]
